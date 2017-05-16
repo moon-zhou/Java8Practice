ac27fed4921f081514025244c40a530deda5f239
moon-zhou <ayimin1989@163.com>
2017-05-16 23:58:54 +0800

    practice simple lambda2

--- src/main/java/com/moonzhou/lambdaTest/ArrayIterationSimple.java
+++ src/main/java/com/moonzhou/lambdaTest/ArrayIterationSimple.java
@@ -2,18 +2,25 @@ package com.moonzhou.lambdaTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ArrayIterationSimple {
 
     public static void main(String[] args) {
-        oldIteration();
+        /*oldIteration();
+
+        newIteration();*/
         
-        newIteration();
+        oldSortIteration();
+        
+        System.out.println("------");
+        
+        newSortIteration();
     }
 
     public static void oldIteration() {
         System.out.println("old iteration");
-        
+
         List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
 
         for (String feature : features) {
@@ -23,8 +30,22 @@ public class ArrayIterationSimple {
 
     public static void newIteration() {
         System.out.println("new iteration");
-        
+
         List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
         features.forEach(n -> System.out.println(n));
+    }
+
+    public static void oldSortIteration() {
+        String[] datas = new String[] { "peng", "zhao", "li" };
+        Arrays.sort(datas);
+        
+        Stream.of(datas).forEach(param -> System.out.println(param));
+    }
+    
+    public static void newSortIteration() {
+        String []datas = new String[] {"peng","zhao","li"};
+        Arrays.sort(datas,(v1 , v2) -> Integer.compare(v1.length(), v2.length()));
+        
+        Stream.of(datas).forEach(param -> System.out.println(param));
     }
 }
