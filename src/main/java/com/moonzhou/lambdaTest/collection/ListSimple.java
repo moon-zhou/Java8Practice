754de50197e653485ca98dd38f76492c28e46897
moon-zhou <ayimin1989@163.com>
2017-05-25 00:00:49 +0800

    list use in common

---
+++ src/main/java/com/moonzhou/lambdaTest/collection/ListSimple.java
@@ -0,0 +1,53 @@
+package com.moonzhou.lambdaTest.collection;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+/**
+ * 
+ * @author moon-zhou
+ *
+ */
+public class ListSimple {
+
+    @Test
+    public void testFilter() {
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+
+        // 创建一个字符串列表，每个字符串长度大于2
+        // 做过滤的时候，通常会丢弃部分，但使用filter()方法则是获得一个新的列表，且其每个元素符合过滤原则。
+        List<String> filtered = strList.stream().filter(x -> x.length() > 2).collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n", strList, filtered);
+    }
+
+    @Test
+    public void testMapToUpper() {
+        // 将字符串换成大写并用逗号链接起来
+        List<String> G7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
+        String G7Countries = G7.stream().map(x -> x.toUpperCase()).collect(Collectors.joining(", "));
+        System.out.println(G7Countries);
+    }
+
+    @Test
+    public void testDistinct() {
+        // 用所有不同的数字创建一个正方形列表
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map(i -> i * i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+    }
+
+    @Test
+    public void testLikeDb() {
+        // 获取数字的个数、最小值、最大值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics stats = primes.stream().mapToInt((x) -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + stats.getMax());
+        System.out.println("Lowest prime number in List : " + stats.getMin());
+        System.out.println("Sum of all prime numbers : " + stats.getSum());
+        System.out.println("Average of all prime numbers : " + stats.getAverage());
+    }
+}
