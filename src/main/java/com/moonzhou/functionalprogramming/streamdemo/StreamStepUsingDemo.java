be3c46d195313f0271a7d2cf745ee7940e9bfc7a
moon-zhou <ayimin1989@163.com>
2019-11-04 23:13:16 +0800

    refactor: ästream using notes

--- src/main/java/com/moonzhou/functionalprogramming/streamdemo/StreamStepUsingDemo.java
+++ src/main/java/com/moonzhou/functionalprogramming/streamdemo/StreamStepUsingDemo.java
@@ -53,14 +53,17 @@ public class StreamStepUsingDemo {
 
         Stream.of(str.split(" ")) // 从字符串数组创建出流对象
                 .filter(s -> s.length() > 2) // 通过流对象的API执行中间操作(filter)，返回的还是流对象
-                .map(s -> s.length()) // 通过返回的流对象再执行中间操作(map)，返回的还是流对象
+                .map(s -> s.length()) // 通过返回的流对象再执行中间操作(map)，返回的还是流对象，从而可以进行链式操作
                 .forEach(System.out::println); // 没有这一步更能说明
     }
 
+    /**
+     * Stream上的操作并不会立即执行，只有等到用户真正需要结果的时候才会执行（惰性求值）。
+     */
     public static void streamFinalOperato() {
         String str = "my name is 007";
         Stream.of(str.split(" "))
-                .peek(System.out::println)
+                .peek(System.out::println) // peek()是一个中间操作，返回的是Stream流对象，只要它不执行最终的操作，这个Stream是不会执行的。
                 .forEach(System.out::println);
     }
 
